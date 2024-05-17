3b33828b9cab17c243b73108ae423dceb5f893fa
Prateek Yashaswi <prateekyashaswi34@gmail.com>
2024-05-17 22:43:31 +0530

    Second Least Character in a string

---
+++ src/main/java/com/example/interviews/questions/SecondLeastCharInAString.java
@@ -0,0 +1,25 @@
+package com.example.interviews.questions;
+
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class SecondLeastCharInAString {
+    public static void main(String[] args) {
+        String s = "Prateek Yashaswi";
+
+        var frequency = s.toLowerCase().chars()
+                .mapToObj(i -> (char) i)
+                .filter(item -> item != ' ')
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(frequency);
+
+        var secondLeast = frequency.values().stream().distinct().sorted().toList().get(1);
+
+        System.out.println("----- ANSWERS -----");
+        frequency.entrySet().stream()
+                .filter(item -> Objects.equals(item.getValue(), secondLeast))
+                .forEach(System.out::println);
+    }
+}
