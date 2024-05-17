110fd092a8ac5f9a3ccedc734edfb49f1258b276
Prateek Yashaswi <prateekyashaswi34@gmail.com>
2024-05-17 23:07:57 +0530

    Proxy Pattern

--- src/main/java/com/example/interviews/designpatterns/structural/proxy/Main.java
+++ src/main/java/com/example/interviews/designpatterns/structural/proxy/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         ExpensiveProcess expensiveProcess = new ExpensiveProcessProxy();
         expensiveProcess.process();
+
+        System.out.println("\nTHIS SHOULD NOT RE-INITIALIZE THE PROCESS AND INSTEAD REUSE THE PREVIOUS ONE");
+        ExpensiveProcess expensiveProcess1 = new ExpensiveProcessProxy();
+        expensiveProcess1.process();
     }
 }
