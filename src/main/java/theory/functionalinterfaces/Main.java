c200ad5b62cdd670c96f747e68b05042f82db393
Prateek Yashaswi <prateekyashaswi34@gmail.com>
2024-05-20 02:27:45 +0530

    Function interfaces with lambda

--- src/main/java/theory/functionalinterfaces/Main.java
+++ src/main/java/theory/functionalinterfaces/Main.java
@@ -4,11 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Calculator add = new AddTwoNumbers();
 
-        int res1 = add.calculate(1,2);
+        int res1 = add.calculate(1, 2);
         add.printResult(res1);
 
         Calculator multiply = new MultiplyTwoNumbers();
-        int res2 = multiply.calculate(5,10);
+        int res2 = multiply.calculate(5, 10);
         multiply.printResult(res2);
+
+        // Can be also used with lambda expressions
+        Calculator sub = (a, b) -> a - b;
+        int res3 = sub.calculate(10, 5);
+        System.out.println("RESULT: " + res3);
     }
 }
