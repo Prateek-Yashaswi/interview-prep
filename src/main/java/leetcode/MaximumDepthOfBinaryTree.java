f8c152471321fe237e754689201dc48f8856fc4b
Prateek Yashaswi <prateekyashaswi34@gmail.com>
2024-05-27 01:25:20 +0530

    Leetcode solutions

---
+++ src/main/java/leetcode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Objects;
+
+public class MaximumDepthOfBinaryTree {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+
+    public int maxDepth(TreeNode root) {
+        return solve(root);
+    }
+
+    private int solve(TreeNode root) {
+        if (Objects.isNull(root)) return 0;
+
+        return Math.max(solve(root.left), solve(root.right)) + 1;
+    }
+
+}
