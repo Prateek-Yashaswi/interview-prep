e6d36e2feb6cbe5628cb92a80ccaba6cd8dc87b3
Prateek Yashaswi <prateekyashaswi34@gmail.com>
2025-03-04 02:01:11 +0530

    Leetcode - ImplementQueuesUsingStack

---
+++ src/main/java/leetcode/easy/ImplementQueuesUsingStack.java
@@ -0,0 +1,55 @@
+package leetcode.easy;
+
+import java.util.Stack;
+
+@SuppressWarnings("all")
+public class ImplementQueuesUsingStack {
+
+    private static class MyQueue {
+        private Stack<Integer> stack;
+
+        public MyQueue() {
+            stack = new Stack<>();
+        }
+
+        public void push(int x) {
+            stack.push(x);
+        }
+
+        public int pop() {
+            var temp = new Stack<Integer>();
+
+            while (!stack.isEmpty()) {
+                temp.push(stack.pop());
+            }
+
+            var removed = temp.pop();
+
+            while (!temp.isEmpty()) {
+                stack.push(temp.pop());
+            }
+
+            return removed;
+        }
+
+        public int peek() {
+            var temp = new Stack<Integer>();
+
+            while (!stack.isEmpty()) {
+                temp.push(stack.pop());
+            }
+
+            var res = temp.peek();
+
+            while (!temp.isEmpty()) {
+                stack.push(temp.pop());
+            }
+
+            return res;
+        }
+
+        public boolean empty() {
+            return stack.isEmpty();
+        }
+    }
+}
