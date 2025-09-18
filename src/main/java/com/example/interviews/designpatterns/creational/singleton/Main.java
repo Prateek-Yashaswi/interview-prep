8f1cf63c3d12472124239acb06d8cd3678e5a4b0
Prateek Yashaswi <prateekyashaswi34@gmail.com>
2025-09-18 14:51:08 +0530

    Design patterns - Singleton

--- src/main/java/com/example/interviews/designpatterns/creational/singleton/Main.java
+++ src/main/java/com/example/interviews/designpatterns/creational/singleton/Main.java
@@ -1,11 +1,17 @@
 package com.example.interviews.designpatterns.creational.singleton;
 
 public class Main {
-    public static void main(String[] args) {
-        var obj1 = SingletonClass.getInstance();
-        obj1.setData(100);
 
-        var obj2 = SingletonClass.getInstance();
-        System.out.println(obj2.getData());
+    public static void main(String[] args) {
+        var room = Room.getInstance();
+        var room1 = Room.getInstance();
+
+        room.setOccupied(true);
+        System.out.println(room.isOccupied());
+        System.out.println(room1.isOccupied());
+
+        room.setOccupied(false);
+        System.out.println(room.isOccupied());
+        System.out.println(room1.isOccupied());
     }
 }
